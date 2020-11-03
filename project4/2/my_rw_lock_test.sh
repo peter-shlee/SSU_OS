e72f10571df028aad20723f22b5bab7884700dce
shlee4290 <shlee4290@naver.com>
2020-11-03 16:08:15 +0900

    project4-2 테스트 코드 shell script 수정

--- project4/2/my_rw_lock_test.sh
+++ project4/2/my_rw_lock_test.sh
@@ -1,31 +1,14 @@
 echo "************** TESTSET: Running Testcases with READER TEST **************"
 gcc my_reader_test.c rw_lock-r-test.c rw_lock.c -o rw_lock-r-test -lpthread
 
-echo "CASE1: Reader Test with 5 reader and 1 writer"
-out=`./rw_lock-r-test 5 1`
-echo -e $out
-
-echo "CASE2: Reader Test with 5 reader and 3 writer"
-out=`./rw_lock-r-test 5 3`
-echo -e $out
-
-echo "CASE3: Reader Test with 5 reader and 5 writer"
+echo "Reader Test with 5 reader and 5 writer"
 out=`./rw_lock-r-test 5 5`
 echo -e $out
 
 echo "************** TESTSET: Running Testcases with WRITER TEST ************** "
 gcc my_writer_test.c rw_lock-w-test.c rw_lock.c -o rw_lock-w-test -lpthread
 
-echo "CASE1: Writer Test with 5 reader and 1 writer"
-out=`./rw_lock-w-test 5 1`
-echo -e $out
-
-
-echo "CASE2: Writer Test with 5 reader and 3 writer"
-out=`./rw_lock-w-test 5 3`
-echo -e $out
-
-echo "CASE3: Writer Test with 5 reader and 5 writer"
+echo "Writer Test with 5 reader and 5 writer"
 out=`./rw_lock-w-test 5 5`
 echo -e $out
 
